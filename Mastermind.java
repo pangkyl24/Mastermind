7c52a517a640bf025560822d8076c3b18014a538
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2022-01-20 09:35:44 -0500

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -36,7 +36,100 @@ public class Mastermind{
 		System.out.println(code);
 	}
 	public void printBoard(){
-		System.out.println(board);
+		for(int i = 0; i < board.size(); i++){
+			for(int j = 0; j < board.get(i).size(); j++){
+				System.out.print(board.get(i).get(j) + " ");
+			}
+			System.out.println();
+		}
+	}	
+	
+	public void scoreRecent(){ //WIP
+		int redPins = 0;
+		int whitePins = 0;
+		
+		for(int i = 0; i < 5; i++){
+			if(board.get(board.size()-1).get(i).equals(code.get(i))){
+				redPins++;
+			}
+		}
+		int rC = 0; 
+		int yC = 0; 
+		int oC = 0; 
+		int gC = 0; 
+		int bC = 0; 
+		int pC = 0; 
+		int wC = 0;
+		for(int i = 0; i < 5; i++){
+			if(board.get(board.size()-1).get(i).equals("R")){
+				rC++;
+			}
+			else if(board.get(board.size()-1).get(i).equals("Y")){
+				yC++;
+			}
+			else if(board.get(board.size()-1).get(i).equals("O")){
+				oC++;
+			}
+			else if(board.get(board.size()-1).get(i).equals("G")){
+				gC++;
+			}
+			else if(board.get(board.size()-1).get(i).equals("B")){
+				bC++;
+			}
+			else if(board.get(board.size()-1).get(i).equals("P")){
+				pC++;
+			}
+			else if(board.get(board.size()-1).get(i).equals("W")){
+				wC++;
+			}
+		}
+		for(int i = 0; i < 5; i++){
+			if(code.get(i).equals("R")){
+				whitePins++;
+				rC--;
+			}
+			else if(code.get(i).equals("Y")){
+				whitePins++;
+				yC--;
+			}
+			else if(code.get(i).equals("O")){
+				whitePins++;
+				oC--;
+			}
+			else if(code.get(i).equals("G")){
+				whitePins++;
+				gC--;
+			}
+			else if(code.get(i).equals("B")){
+				whitePins++;
+				bC--;
+			}
+			else if(code.get(i).equals("P")){
+				whitePins++;
+				pC--;
+			}
+			else if(code.get(i).equals("W")){
+				whitePins++;
+				wC--;
+			}
+		}
+		whitePins-=redPins;
+		
+		board.get(board.size()-1).add("{---- ");
+		for(int i = 0; i < 5; i++){
+			if(redPins!=0){
+				redPins--;
+				board.get(board.size()-1).add("O");
+			}
+			else if(whitePins!=0){
+				whitePins--;
+				board.get(board.size()-1).add("X");
+			}
+			else{
+				board.get(board.size()-1).add("-");
+			}
+		}
+		board.get(board.size()-1).add(" ----}");
 	}
 	
 	public void addGuess(String inp1, String inp2, String inp3, String inp4, String inp5){
@@ -50,3 +143,4 @@ public class Mastermind{
 	}
 	
 }
+
